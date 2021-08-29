e3dffea3dd9ad49a6e5df2e4b9aadc1cd6a928a7
Ainhoa Suárez Sánchez <ainhoass89@gmail.com>
2021-08-29 21:23:47 +0200

    app refactor

--- src/App.js
+++ src/App.js
@@ -10,44 +10,44 @@ class App extends Component {
 
   constructor(props) {
     super(props);
-    var lotsOfEmployees=[]
-    for (var i=1;i<=1000;i++){
-      lotsOfEmployees=lotsOfEmployees.concat(employeeArray)
+    var lotsOfEmployees = [];
+    for (var i = 1; i <= 1000; i++) {
+      lotsOfEmployees = lotsOfEmployees.concat(employeeArray);
+      lotsOfEmployees._id = i;
     }
-    lotsOfEmployees.forEach(function(emp,i){
-      emp._id=i;
-    })
     this.state = {
-      employees:lotsOfEmployees,
-      searchText:''
+      employees: lotsOfEmployees,
+      searchText: ''
     };
   }
-  
+
   handleSearch(searchText) {
-    this.setState({searchText: searchText});
+    this.setState({ searchText: searchText });
   }
 
-  update(day,event){
-    var currentEmployees=this.state.employees;
-    var selectedEmployee=Math.floor(event.nativeEvent.offsetY/45);
-    var currentDayFormatted=day.format('YYYY-MM-DD');
-    var nextDayFormatted=moment(currentDayFormatted).add(1,'days').format('YYYY-MM-DD');
-    var newelement={from:currentDayFormatted,to:nextDayFormatted,type:'vacation'};
+  update(day, event) {
+    var currentEmployees = this.state.employees;
+    var selectedEmployee = Math.floor(event.nativeEvent.offsetY / 45);
+    var currentDayFormatted = day.format('YYYY-MM-DD');
+    var nextDayFormatted = moment(currentDayFormatted).add(1, 'days').format('YYYY-MM-DD');
+    var newelement = { from: currentDayFormatted, to: nextDayFormatted, type: 'vacation' };
     currentEmployees[selectedEmployee].events.push(newelement);
 
     this.setState({
-      employees:currentEmployees
+      employees: currentEmployees
     });
   }
-  
+
   render() {
     return (
       <div className="gantt">
-        
         <Search handleSearch={this.handleSearch.bind(this)}></Search>
-        <Employees employees={this.state.employees}  searchText={this.state.searchText} />
-        <GanttChart searchText={this.state.searchText} employees={this.state.employees} handleOpenModal={this.handleOpenModal} handleUpdate={this.update.bind(this)}/>
-          
+        <Employees employees={this.state.employees} searchText={this.state.searchText} />
+        <GanttChart 
+          searchText={this.state.searchText} 
+          employees={this.state.employees} 
+          handleOpenModal={this.handleOpenModal} 
+          handleUpdate={this.update.bind(this)} />
       </div>
     );
   }
